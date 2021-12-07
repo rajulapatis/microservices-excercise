e09c540267cae089662a144ad94d2fb06d136095
Srini Rajulapati <rajulapati@yahoo.com>
2021-12-07 15:43:07 -0600

    commit on 12/7

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a3c2af2263b6b49ba971be74011677c3-2070710770.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
